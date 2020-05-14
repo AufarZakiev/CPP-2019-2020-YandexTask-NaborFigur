f74c7fa6feb85192eb5c2269b319d08a61c1f2c6
camila <alimacam207@gmail.com>
2020-05-14 17:57:10 +0300

    HW

--- CPP-2019-2020-TemplateCPP.pro
+++ CPP-2019-2020-TemplateCPP.pro
@@ -1,7 +1,11 @@
 TEMPLATE = app
-CONFIG += console c++11
+CONFIG += console c++17
 CONFIG -= app_bundle
 CONFIG -= qt
 
 SOURCES += \
+        figures.cpp \
         main.cpp
+
+HEADERS += \
+    figures.h
--- main.cpp
+++ main.cpp
@@ -1,9 +1,61 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <iomanip>
+#include <figures.h>
 
 using namespace std;
 
+shared_ptr<Figure> CreateFigure(istringstream& is)
+{
+  shared_ptr<Figure> figure;
+  string type;
+  is >> type;
+  if (type == "TRIANGLE")
+  {
+    int a, b, c;
+    is >> a >> b >> c;
+    figure = make_shared<Triangle>(a, b, c);
+  }
+  else if (type == "RECT")
+  {
+    int width, height;
+    is >> width >> height;
+    figure = make_shared<Rect>(width, height);
+  }
+  else if (type == "CIRCLE")
+  {
+    int r;
+    is >> r;
+    figure = make_shared<Circle>(r);
+  }
+  else
+    throw "Wrong type of figure is written!";
+  return figure;
+};
+
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  vector<shared_ptr<Figure>> figures;
+  for (string line; getline(cin, line);)
+  {
+    istringstream is(line);
+
+    string command;
+    is >> command;
+    if (command == "ADD")
+    {
+      figures.push_back(CreateFigure(is));
+    }
+    else if (command == "PRINT")
+    {
+      for (const auto& current_figure : figures)
+      {
+        cout << fixed << setprecision(3) << current_figure->Name() << " " << current_figure->Perimeter() << " "
+             << current_figure->Area() << endl;
+      }
+    }
+  }
+  return 0;
 }
